feaf50ebf70a638c5be0f468da238067bde62a01
nntruong97 <nguyennhattruongspkt@gmail.com>
2021-12-10 21:43:56 +0700

    interpolation - databinding - twowaybinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  inputText = 'text';
+  user = {
+    name: 'Truong',
+    age: 25,
+  };
+
+  handler($event) {
+    console.log('1',$event);
+  }
 }
